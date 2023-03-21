760914ada95a1cdac943ecbf4cf37672ee1cc60c
blackwut <albertoottimo@gmail.com>
2023-03-21 12:58:08 +0100

    initial commit of FNodeTask

--- README.md
+++ README.md
@@ -1,9 +1,41 @@
 # FFpga
-offloading from FastFlow nodes to Vitis kernels on Alveo FPGAs
+Experimental FastFlow node called FNodeTask to offload computation of Vitis HLS kernels on Alveo FPGAs.
 
-### ff_node_fpga
-The ff_node_fpga can be used in any place where you need an ff_node/ff_node_t. It offloads tasks to pre-compiled kernels on an Alveo FPGA 
+### FNodeTask
+The FNodeTask can be used in any place where you need an ff_node/ff_node_t.
+It offloads tasks to pre-compiled kernels on an Alveo FPGA
 
-## Sample code (so far)
-### pip2 
-pip2 kernelname bitstream executes a vadd like kernel on a range of vector pairs randomly generated in a three stage pipeline
+## Sample code
+The `vadd_cus` program executes a VectorAdd (vadd) kernel composed of multiple Compute Units (CUs).
+
+### Compile `vadd` kernel
+```
+cd kernels/vadd_cus
+make all TARGET=hw # sw_emu or hw_emu
+```
+
+### Compile `vadd_cus` host
+```
+cd test
+make vadd_cus
+```
+
+### Run
+You can use tests contained in the `Makefile` as follows:
+
+```
+$ make test_pipe    # runs a pipeline containing FNodeTask
+$ make test_farm    # runs a farm of pipeline containing FNodeTask
+```
+
+By executing `vadd_cus` without parameters it will show:
+
+```
+$ ./vadd_cus
+Usage:
+        ./vadd_cus [file.xclbin] [kernel_name] [num_workers] [chain_tasks] [vec_elems] [vec_nums]
+
+Example:
+        ./vadd_cus vadd.xclbin vadd 2 0 $((1 << 10)) $((1 << 6))
+```
+
--- fpga/fdevice.hpp
+++ fpga/fdevice.hpp
@@ -8,130 +8,167 @@
 #include <CL/cl2.hpp>
 #include <iostream>
 #include <fstream>
-#include <iostream>
 #include <string>
 
-#define OCL_CHECK(error, call) \
-call; \
-if (error != CL_SUCCESS) { \
-    printf("%s:%d Error calling " #call ", error code is: %d\n", __FILE__, __LINE__, error); \
-    exit(EXIT_FAILURE); \
+#define OCL_CHECK(error, call)                                                                   \
+    call;                                                                                        \
+    if (error != CL_SUCCESS) {                                                                   \
+        printf("%s:%d Error calling " #call ", error code is: %d\n", __FILE__, __LINE__, error); \
+        exit(EXIT_FAILURE);                                                                      \
+    }
+
+auto get_time() { return std::chrono::high_resolution_clock::now(); }
+auto time_diff_ms(std::chrono::high_resolution_clock::time_point start, std::chrono::high_resolution_clock::time_point end) {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count(); 
 }
 
-
-class FDevice {
-
+class FDevice
+{
 private:
-    std::string bs;
-    std::string kn;
+    cl::Platform platform_;
+    cl::Program  program_;
 
-    cl_int err;
-    cl::Device device;
-    cl::Context context;
-    cl::Program program;
-
-
-public:
-
-    FDevice(std::string bs, std::string kn)
-    : bs(bs)
-    , kn(kn)
+    void load_platform()
     {
-        bool found_device = false;
+        cl_int err;
+
+        bool found_platform = false;
         std::vector<cl::Platform> platforms;
-        cl::Platform::get(&platforms);
-        for(size_t i = 0; i < platforms.size(); ++i) {
+        OCL_CHECK(err, err = cl::Platform::get(&platforms));
+        for (int i = 0; i < platforms.size(); ++i) {
             cl::Platform platform = platforms[i];
-            std::string platformName = platform.getInfo<CL_PLATFORM_NAME>();
+            OCL_CHECK(err, std::string platformName = platform.getInfo<CL_PLATFORM_NAME>(&err));
             if (platformName == "Xilinx") {
-                std::vector<cl::Device> devices;
-                platform.getDevices(CL_DEVICE_TYPE_ACCELERATOR, &devices);
-                if (devices.size()){
-                    device = devices[0];
-                    found_device = true;
-                    break;
-                }
+                platform_ = platform;
+                found_platform = true;
+                break;
             }
         }
-
-        if (found_device) {
-            std::cerr << "MDLOG: Device found" << std::endl;
-        } else {
-            std::cerr << "MDLOG: Device NOT found" << std::endl;
+        if (!found_platform) {
+            std::cerr << "ERROR: failed to find Xilinx platform" << std::endl;
             exit(-1);
         }
-
-        context = createContext();
-        program = createProgram();
     }
 
-    cl::Context createContext()
+    void load_device(int device_id)
     {
-        cl::Context _context = cl::Context(device, NULL, NULL, NULL, &err);
-        if(err != CL_SUCCESS) {
-            std::cerr << "MDLOG: Context CANNOT be created" << std::endl;
+        cl_int err;
+
+        bool found_device = false;
+        std::vector<cl::Device> devices;
+        OCL_CHECK(err, err = platform_.getDevices(CL_DEVICE_TYPE_ACCELERATOR, &devices));
+
+        if (devices.size() == 0) {
+            std::cerr << "ERROR: failed to find a Xilinx device" << std::endl;
             exit(-2);
         }
-        std::cerr << "MDLOG: got context " << std::endl;
 
-        return _context;
-    }
-
-    cl::Context getContext()
-    {
-        return context;
-    }
-
-    cl::Program createProgram()
-    {
-        std::cout << "INFO: Reading " << bs << std::endl;
-        FILE* fp;
-        if ((fp = fopen(bs.c_str(), "r")) == nullptr) {
-            printf("ERROR: %s xclbin not available please build\n", bs.c_str());
-            exit(EXIT_FAILURE);
-        }
-        // Loading XCL Bin into char buffer
-        std::cout << "Loading: '" << bs.c_str() << "'\n";
-        std::ifstream bin_file(bs.c_str(), std::ifstream::binary);
-        bin_file.seekg(0, bin_file.end);
-        auto nb = bin_file.tellg();
-        bin_file.seekg(0, bin_file.beg);
-        std::vector<unsigned char> buf;
-        buf.resize(nb);
-        bin_file.read(reinterpret_cast<char*>(buf.data()), nb);
-
-        // Creating Program from Binary File
-        cl::Program::Binaries bins{{buf.data(), buf.size()}};
-
-        std::vector<cl::Device> devices{device};
-        // devices.push_back(device);
-
-        cl::Program program = cl::Program(context, devices, bins, nullptr, &err);
-        if(err != CL_SUCCESS) {
-            std::cout << "Error loading program code from xclbin" << std::endl;
-            exit(-2);
-        }
-        return program;
-    }
-
-
-    cl::Kernel createKernelInstance()
-    {
-        cl::Kernel krnl = cl::Kernel(program, kn.c_str(), &err);
-        if(err != CL_SUCCESS) {
-            std::cout << "Error creating kernel from xclbin" << std::endl;
+        if (device_id < 0) device_id = 0;
+        if (device_id < devices.size()) {
+            device_ = devices[device_id];
+            found_device = true;
+        } else {
+            std::cerr << "ERROR: failed to find the Xilinx device[" << device_id << "]" << std::endl;
             exit(-3);
         }
-        return krnl;
     }
 
-    cl::CommandQueue createQueueInstance()
+    void load_context()
     {
-        cl::CommandQueue q = cl::CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE | CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE, &err);
-        if (err != CL_SUCCESS) {
-            std::cerr << "Error creating CommandQueue" << std::endl;
-            exit(-1);
+        cl_int err;
+        OCL_CHECK(err, context_ = cl::Context(device_, NULL, NULL, NULL, &err));
+    }
+
+    void load_program(std::string bitstream)
+    {
+        cl_int err;
+
+        std::cout << "Loading " << bitstream << " bitstream...\n" << std::endl;
+
+        FILE * fp;
+        if ((fp = fopen(bitstream.c_str(), "r")) == nullptr) {
+            std::cerr << "ERROR: " << bitstream << " xclbin not available" << std::endl;
+            exit(-4);
         }
-        return q;
+
+        // Load xclbin
+        std::ifstream bin_file(bitstream, std::ifstream::binary);
+        bin_file.seekg (0, bin_file.end);
+        unsigned nb = bin_file.tellg();
+        bin_file.seekg (0, bin_file.beg);
+        char * buf = new char[nb];
+        bin_file.read(buf, nb);
+
+        cl::Program::Binaries bins;
+        bins.push_back({buf, nb});
+
+        std::vector<cl::Device> program_devices;
+        program_devices.push_back(device_);
+
+        OCL_CHECK(err, program_ = cl::Program(context_, program_devices, bins, NULL, &err));
+    }
+
+public:
+    cl::Device       device_;
+    cl::Context      context_;
+    cl::CommandQueue queue_;
+
+    FDevice(std::string bitstream, int device_id = 0)
+    {
+        load_platform();
+        load_device(device_id);
+        load_context();
+        load_program(bitstream);
+        queue_ = createCommandQueue(false, true);
+    }
+
+    cl::Kernel createKernel(std::string kernel_name)
+    {
+        cl_int err;
+        cl::Kernel kernel = cl::Kernel(program_, kernel_name.c_str(), &err);
+        if (err != CL_SUCCESS) {
+            std::cerr << "ERROR: failed to create an instance of " << kernel_name << " kernel" << std::endl;
+            exit(-5);
+        }
+        return kernel;
+    }
+
+    cl::CommandQueue createCommandQueue(bool profiling = false, bool out_of_order = false)
+    {
+        cl_int err;
+        cl::CommandQueue queue;
+        OCL_CHECK(err, queue = cl::CommandQueue(context_,
+                                                device_,
+                                                (profiling ? cl::QueueProperties::Profiling : cl::QueueProperties::None)
+                                                | (out_of_order ? cl::QueueProperties::OutOfOrder : cl::QueueProperties::None),
+                                                &err));
+        return queue;
+    }
+
+    bool is_emulation()
+    {
+        char * xcl_mode = getenv("XCL_EMULATION_MODE");
+        if (xcl_mode != nullptr) {
+            return true;
+        }
+        return false;
+    }
+
+    bool is_sw_emulation()
+    {
+        char * xcl_mode = getenv("XCL_EMULATION_MODE");
+        if ((xcl_mode != nullptr) && !strcmp(xcl_mode, "sw_emu")) {
+            return true;
+        }
+        return false;
+    }
+
+    bool is_hw_emulation()
+    {
+        char * xcl_mode = getenv("XCL_EMULATION_MODE");
+        if ((xcl_mode != nullptr) && !strcmp(xcl_mode, "hw_emu")) {
+            return true;
+        }
+        return false;
     }
 };
